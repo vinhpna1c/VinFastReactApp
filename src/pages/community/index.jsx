2761af7ee24e90e698411636d5e4dcec2ca2146c
vinhpna1c <114041994+vinhpna1c@users.noreply.github.com>
2023-04-04 09:58:03 +0700

    update get post amity

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -1,22 +1,32 @@
-import {SafeAreaView, ScrollView, Text} from 'react-native';
-import {useState,useEffect}from 'react'
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { useState, useEffect } from 'react'
 import AmityPostController from '../../controller/amity/amity_post_controller';
 import AmityFeedStore from '../../store/feed/AmityFeedStore';
-export default function CommunityScreen(){
-    useEffect(()=>{
-        const controller=new AmityPostController();
-        controller.queryGlobalFeed();
-    });
- const store=new AmityFeedStore();
- console.log("Load frome community");
- console.log(store.posts);
-    return(
-   <SafeAreaView>
-    <ScrollView>
-        {store.posts.map(e=>(<Text>
-            e
-        </Text>))}
-    </ScrollView>
-   </SafeAreaView>
+import {runQuery,createQuery,queryGlobalFeed} from '@amityco/ts-sdk'
+export default function CommunityScreen() {
+    const [posts, setPosts] = useState([]);
+
+
+
+    const getGlobalFeed = () => {
+        const query = createQuery(queryGlobalFeed);
+        runQuery(query, ({ data: postList, ...options }) => {
+            setPosts(postList);
+            console.log(posts[0]);
+        });
+    }
+
+    useEffect(() => {
+        getGlobalFeed();
+    },[]);
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                {posts.map(e => (<Text>
+                    {e['_id']}
+                </Text>))}
+            </ScrollView>
+        </SafeAreaView>
     );
 }
