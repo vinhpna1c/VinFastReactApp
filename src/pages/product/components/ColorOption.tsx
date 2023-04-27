35d9c71b10ceec7651e72dfaa38b9b7dd4f8067a
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2023-04-27 15:44:18 +0700

    temp update

--- src/pages/product/components/ColorOption.tsx
+++ src/pages/product/components/ColorOption.tsx
@@ -1,26 +1,30 @@
-import { View ,Text, TouchableOpacity} from "react-native";
+/** @format */
+
+import { TouchableOpacity } from "react-native";
 import styles from "../styles";
-import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import AntDesignIcon from "react-native-vector-icons/AntDesign";
 
-type ColorOptionProps={
-    color?:string,
-    selected?:boolean,
-    onTap?: Function,
+type ColorOptionProps = {
+  color?: string;
+  selected?: boolean;
+  onTap?: Function;
+  image: any;
+};
+
+function ColorOption(props: ColorOptionProps): JSX.Element {
+  // props.color??='white';
+  return (
+    <TouchableOpacity
+      style={{ ...styles.colorOption, backgroundColor: props.color ?? "white" }}
+      onPress={() => {
+        if (props.onTap != undefined) {
+          props.onTap();
+        }
+      }}
+    >
+      {props.selected && <AntDesignIcon size={22} name="check" />}
+    </TouchableOpacity>
+  );
 }
 
-function ColorOption(props:ColorOptionProps):JSX.Element
-{
-    // props.color??='white';
-    return (
-        <TouchableOpacity style={{...styles.colorOption,backgroundColor:props.color??'white'}} onPress={()=>{
-            if(props.onTap!=undefined)
-            {
-                props.onTap();
-            }
-        }}> 
-            {props.selected&&<AntDesignIcon size={22} name='check' />}
-        </TouchableOpacity>
-    )
-}
-
-export default ColorOption
+export default ColorOption;
